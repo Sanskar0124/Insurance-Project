544ca8bbd7975dc954fabc73585a5481ce5b976c
Sanskar-Sakhareliya <sanskar.sakhareliya@bjtmail.com>
2024-03-30 15:38:24 +0530

    Initial Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className='px-4 lg:px-14 max-w-screen-2cl mx-auto min-h-screen h-screen'>
                 <Carousel >
                     <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
-                        <img src={'https://lottie.host/20ebb9e8-e9f9-4263-b7fc-b16111cb5199/HFrMHz0lLC.lottie'} alt='' />
+                        <img src={'https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/gifs/health-insurance.gif?raw=true'} alt='' />
                     </div>
                     <div className="flex h-full items-center justify-center">
                         Slide 2
