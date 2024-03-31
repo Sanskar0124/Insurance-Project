acc7a18e201634e1e54b49d96b93d514742696a3
Sanskar-Sakhareliya <sanskar.sakhareliya@bjtmail.com>
2024-03-31 20:04:49 +0530

    Added Logos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
         <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0'>
             <nav className={`py-4 ls:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border bg-white duration-300" : ""}`}>
                 <div className='flex justify-between items-center text-base gap-8'>
-                    <a href="" className='text-2xl font-semibold flex items-center space-x-3'><img src={''}
+                    <a href="" className='text-2xl font-semibold flex items-center space-x-3'><img src={Bio.logo}
                         alt="" className='w-10 inline-block items-center' /><span className='text-[#263238]
               '>{Bio.name}</span></a>
 
--- src/data/constatnts.jsx
+++ src/data/constatnts.jsx
@@ -1,3 +1,4 @@
 export const Bio = {
     name: "SureCare",
+    logo: 'https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/logos/logo.png?raw=true'
 };
