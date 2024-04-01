8a39d27db4cf595eee9c13b851744ce6f29e2ee9
Sanskar-Sakhareliya <sanskar.sakhareliya@bjtmail.com>
2024-04-01 13:56:03 +0530

    Added Images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
+import { Bio } from '../data/constatnts'
 
 export default function About() {
     return (
         <div id='about'>
             {/* about text */}
-            <div className='px-4 1g:px-14 max-w-screen-2x1 mx-auto my-8'>
+            <div className='px-4 1g:px-14 max-w-screen-2xl mx-auto mb-10'>
                 <div className='md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12'>
                     <div>
-                        <img src={'https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/images/health_insurance.png?raw=true'} alt="" width={400} />
+                        <img src={'https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/images/health_insurance2.png?raw=true'} alt="" width={400} />
                     </div>
                     <div className='md:w-3/5 mx-auto'>
-                        <h2 className='text-4xl  text-neutralDGrey font-semibold mb-4 md:w-4/5'>The unseen of
-                            spending three years at Pixelgrade</h2>
-                        <p className='md:w-3/4 text-sm ☐ text-neutralGrey mb-8'>Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est
-                            varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
-                            tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa
-                            pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+                        <h2 className='text-4xl  text-neutralDGrey font-semibold mb-4 md:w-4/5'>Your Trusted Insurance Partner</h2>
+                        <p className='md:w-3/4 text-sm ☐ text-neutralGrey mb-8'>
+                            Welcome to {Bio.name}, where your peace of mind is our priority. With personalized solutions and expert guidance, we're dedicated to ensuring you have the coverage you need. Trust in our expertise and commitment to your financial security. At {Bio.name}, we're not just your insurance provider; we're your trusted partner in safeguarding your future.
+                        </p>
                         <button className='btn-primary'>Learn More</button>
                     </div>
                 </div>
@@ -26,8 +24,8 @@ export default function About() {
             <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16'>
                 <div className='flex flex-col md:flex-row justify-between items-center gap-8'>
                     <div className='md:w-1/2'>
-                        <h2 className='text-4xl text-neutralDGrey font-semibold mb-4 md: w-2/3'>Helping a local <br />
-                            <span className='text-brandPrimary'>business reinvent itself</span></h2>
+                        <h2 className='text-4xl text-neutralDGrey font-semibold mb-4 md:w-2/3'>Our Commitment:<br />
+                            <span className='text-brandPrimary text-1xl'>Empowering Financial Security</span></h2>
                         <p>We reached here with our hard work and dedication</p>
                     </div>
 
