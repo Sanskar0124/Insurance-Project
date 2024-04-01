a15e81e41750e21e8ec28ee785df6eab6a9f8fb0
Sanskar-Sakhareliya <sanskar.sakhareliya@bjtmail.com>
2024-04-01 12:29:14 +0530

    Added Images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Services() {
 
     const services = [
-        { id: 1, title: "Expert Consultation", descroption: "Speak with our experienced advisors for personalized insurance guidance and tailored solutions", image: "https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/images/expert_consultation.png?raw=true" },
+        { id: 1, title: "Expert Consultation", descroption: "Speak with our experienced advisors for personalized insurance guidance and tailored solutions", image: "https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/services%20images/presentation_3797362.png?raw=true" },
         { id: 2, title: "Customized Plans", descroption: "Explore our range of insurance plans tailored to your unique needs, lifestyle, and budget", image: "https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/images/customizable_plan.png?raw=true" },
         { id: 3, title: "24/7 Support", descroption: "Access our dedicated team for round-the-clock assistance with all your insurance inquiries and needs", image: "https://github.com/Sanskar0124/Insurance-Project/blob/master/src/assets/images/support.png?raw=true" },
     ]
     return (
-        <div className='md:px-14 px-4 py-16 max-w-screen-2x1 mx-auto' id='service'>
+        <div className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto' id='service'>
             {/* <div className='text-center my-8'>
-                <h2 className='text-4x1 text-neutralDGrey font-semibold mb-2'>Our Clients</h2>
+                <h2 className='text-4xl text-neutralDGrey font-semibold mb-2'>Our Clients</h2>
                 <p className='text-neutralGrey'>We have been working with some Fortune 500+ clients</p>
                 <div className="my-12 flex flex-wrap justify-between items-center gap-8">
                     <img src="" alt="Company 1" />
@@ -25,7 +25,7 @@ export default function Services() {
 
             {/* services card  */}
             <div className='mt-20 md:w-1/2 mx-auto text-center'>
-                <h2 className='text-4x1 text-neutralDGrey font-semibold mb-3'>Discover the SureCare Advantage</h2>
+                <h2 className='text-4xl text-neutralDGrey font-semibold mb-3'>Discover the SureCare Advantage</h2>
                 <p className='text-neutralGrey'>Experience peace of mind with our expert guidance and personalized insurance solutions</p>
             </div>
 
@@ -37,10 +37,10 @@ export default function Services() {
             hover:border-indigo-700 transition-all duration-300 flex items-center justify-center
             h-full'>
                         <div>
-                            <div className=' bg-[#E8F5E9] mb-4 h-14 w-14 mx-auto rounded-t1-3x1 rounded-br-3x1'>
-                                <img src={service?.image} alt="" className='-ml-5' />
+                            <div className=' bg-[#E8F5E9] mb-4 h-14 w-14 mx-auto rounded-t1-3xl rounded-br-3xl'>
+                                <img src={service?.image} alt="" className='-ml-5' color='red' />
                             </div>
-                            <h4 className='text-2x1 font-bold text-neutralDGrey mb-2 px-2'>{service.title}</h4>
+                            <h4 className='text-2xl font-bold text-neutralDGrey mb-2 px-2'>{service.title}</h4>
                             <p className='text-sm ☐ text-neutralGrey'>{service.descroption}</p>
                         </div>
                     </div>)
